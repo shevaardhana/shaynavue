e3472f0d6ad4a3ecac6ba1329a979dd97987994a
Sheva Ardhana <shevaardhana26@gmail.com>
2021-03-23 21:53:37 +0700

    hasil latihan -1

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <Header />
+    <CarouselShayna />
+    <CarouselBannerShayna />
+    <InstagramShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/HeaderShayna.vue';
+import CarouselShayna from '../components/CarouselShayna.vue';
+import CarouselBannerShayna from '../components/CarouselBannerShayna.vue';
+import InstagramShayna from '../components/InstagramShayna.vue';
+import PartnerShayna from '../components/PartnerShayna.vue';
+import FooterShayna from '../components/FooterShayna.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    CarouselShayna,
+    CarouselBannerShayna,
+    InstagramShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+}
+</script>
